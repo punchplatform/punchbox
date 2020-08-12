a061017214d00512bdae937dd0322defe074517c
rafael.leveque <rafael.leveque@mythalesgroup.io>
2020-08-12 10:55:12 +0200

    Update local_integration.sh

--- punch/configurations/validation/binutils/local_integration.sh
+++ punch/configurations/validation/binutils/local_integration.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# This shell launch automaticaly punchbox with current pp-punch. Results are published in slack 
-# Two vars are mandatory and specific to this script: 
+# This shell launch automatically punchbox with current pp-punch. Results are published in slack
+# Two vars are mandatory and specific to this script:
 #   - PUNCHBOX_DIR=/home/punch/workspace/pp-integration-vagrant
 #   - PUNCH_DIR=/home/punch/workspace/craig/pp-punch
-# Vars from current user are also used by maven and pex when building repository. Be sure that they are available 
+# Vars from current user are also used by maven and pex when building repository. Be sure that they are available
 # if you use a non-interactive shell (cron for example)
 
 RED="\033[31m"
@@ -28,18 +28,18 @@ mvn clean install -DskipTests
 cd $PUNCHBOX_DIR
 git checkout $1
 git pull
-DEPLOYER_ZIP=$(ls -of $PUNCH_DIR/pp-packaging/punchplatform-deployer/target/punchplatform-deployer-*.zip)
-PUNCHCONF=$(realpath $PUNCH_DIR/pp-packaging/punchplatform-standalone/target/tmp/punchplatform-resources-*/conf/)
+DEPLOYER_ZIP=$(ls -of $PUNCH_DIR/packaging/punchplatform-deployer/target/punchplatform-deployer-*.zip)
+VALIDATION_CONF=$(realpath $PUNCHBOX_DIR/punch/configurations/validation)
 
-echo -e "${GREEN}INFO:${RESET} Launch punchbox with deployer : $DEPLOYER_ZIP and config : $PUNCHCONF"
+echo -e "${GREEN}INFO:${RESET} Launch punchbox with deployer : $DEPLOYER_ZIP and validation config : $VALIDATION_CONF"
 
 make install
 source activate.sh
-punchbox --deployer $DEPLOYER_ZIP --config configurations/complete_punch_32G.json --punch-conf $PUNCHCONF --generate-vagrantfile --start-vagrant
+punchbox --deployer $DEPLOYER_ZIP --platform-config-file configurations/complete_punch_32G.json --punch-validation-config "$VALIDATION_CONF" --generate-vagrantfile --start-vagrant
 source $PUNCHBOX_DIR/activate.sh
 punchplatform-deployer.sh --generate-platform-config --templates-dir $PUNCHBOX_DIR/punch/platform_template/ --model $PUNCHBOX_DIR/punch/build/model.json
 punchplatform-deployer.sh -gi
-punchplatform-deployer.sh deploy -u vagrant
+punchplatform-deployer.sh --deploy -u vagrant
 
 echo -e "${GREEN}INFO:${RESET} Launch check platform "
 
