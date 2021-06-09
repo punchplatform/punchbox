43808ed0ccbce418072e64f0c39f8bc3a398d448
louis cailliot <louis.cailliot@thalesgroup.com>
2021-06-09 09:55:55 +0200

    light punchbox modsecurity configuration

--- bin/punchbox.py
+++ bin/punchbox.py
@@ -3,7 +3,10 @@ import json
 import logging
 import os
 import subprocess
+import sys
+from json import JSONDecodeError
 from sys import exit
+from typing import Dict
 
 import jinja2
 
@@ -20,7 +23,7 @@ PUNCHBOX_TEMPLATES_DIR = os.environ.get('PUNCHBOX_TEMPLATES_DIR')
 # PUNCHBOX FILES
 VAGRANT_FILE = f"{PUNCHBOX_BUILD_DIR}/Vagrantfile"
 MODEL_FILE = f"{PUNCHBOX_BUILD_DIR}/model.json"
-SETTINGS_FILE = f"{PUNCHBOX_BUILD_DIR}/punchplatform-deployment.settings"
+SETTINGS_FILE = f"{PUNCHPLATFORM_CONF_DIR}/punchplatform-deployment.settings"
 
 components = ["punch", "minio", "zookeeper", "spark", "elastic", "opendistro_security", "operator", "binaries",
               "analytics-deployment", "analytics-client", "shiva", "gateway", "storm", "kafka", "logstash",
@@ -35,7 +38,7 @@ def load_platform_config(platform_config_file: str):
     :return: the json object generated from the config file
     """
     with open(platform_config_file) as f:
-        logging.info('Loading platform configuration from file %s', platform_config_file)
+        logging.debug(' Loading platform configuration from file %s', platform_config_file)
         return json.load(f)
 
 
@@ -53,20 +56,26 @@ def get_versions():
     return data
 
 
-def render_and_write(template_dir, template_file, target_file, *args, **kwargs):
-    """
-    Use jinja2 to generate a file from a given template
-    :param template_dir: Where the template file is located
-    :param template_file: The template file name
-    :param target_file: the output file to write
-    """
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir)) \
-        if isinstance(template_dir, str) else template_dir
+def write_json(json_str: str, output: str):
+    try:
+        parsed_json_str = json.loads(json_str)
+    except JSONDecodeError as err:
+        bad_json = "/tmp/punchbox_bad.json"
+        with open(bad_json, "w") as f:
+            f.write(json_str)
+        logging.error(f" Bad Json : {err}")
+        print(f"Check {bad_json}")
+        sys.exit(1)
+
+    with open(output, 'w') as f:
+        f.write(json.dumps(parsed_json_str, indent=4, sort_keys=False))
+
+
+def render(template_dir: str, template_file: str, model: Dict):
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir)) if isinstance(template_dir, str) \
+        else template_dir
     template = env.get_template(template_file)
-    rendered_content = template.render(*args, **kwargs)
-    target = open(target_file, "w+")
-    target.write(rendered_content)
-    target.close()
+    return template.render(**model)
 
 
 def generate_vagrantfile(platform_config):
@@ -75,12 +84,13 @@ def generate_vagrantfile(platform_config):
     The vagrant file is generated inside $PUNCHBOX_BUILD_DIR/vagrant
     :param platform_config: the platform configuration
     """
-    render_and_write(PUNCHBOX_TEMPLATES_DIR, VAGRANT_TEMPLATE_NAME, VAGRANT_FILE,
-                     targets=platform_config["targets"])
-    logging.info('Vagrantfile successfully generated in %s', VAGRANT_FILE)
+    vagrantfile_str = render(PUNCHBOX_TEMPLATES_DIR, VAGRANT_TEMPLATE_NAME, platform_config)
+    with open(VAGRANT_FILE, "w") as f:
+        f.write(vagrantfile_str)
+    logging.debug(' Vagrantfile successfully generated in %s', VAGRANT_FILE)
 
 
-def generate_model(platform_config):
+def generate_model(platform_config) -> Dict:
     model = {}
     data = get_versions()
 
@@ -92,24 +102,23 @@ def generate_model(platform_config):
     if 'tls' in platform_config['punch'] and bool(platform_config['punch']['tls']):
         model['security_dir'] = f"{PUNCHPLATFORM_CONF_DIR}/security"
 
-    model = json.dumps({**model, **platform_config['punch']}, indent=4, sort_keys=True)
+    model_str = json.dumps({**model, **platform_config['punch']}, indent=4, sort_keys=True)
     model_file = open(MODEL_FILE, "w+")
-    model_file.write(model)
+    model_file.write(model_str)
     model_file.close()
-    logging.info('Platform model file successfully generated in %s', MODEL_FILE)
-    return model
+    logging.debug(' Platform model file successfully generated in %s', MODEL_FILE)
+    return json.loads(model_str)
 
 
-def generate_deployment_settings():
+def generate_deployment_settings(model: Dict):
     """
     Call punchplatform-deployer.sh to generate the deployment settings file.
     This file is generated using the model file for input values, according to the jinja template in the deployment
     template directory.
     """
-    cmd = f"punchplatform-deployer.sh --generate-platform-config --templates-dir {PUNCHBOX_TEMPLATES_DIR}/settings" \
-          f" --model {MODEL_FILE}"
-    subprocess.check_output(cmd, shell=True)
-    logging.info(' Deployment settings successfully generated in %s', VAGRANT_FILE)
+    settings_str = render(PUNCHBOX_TEMPLATES_DIR, SETTINGS_TEMPLATE_NAME, model)
+    write_json(settings_str, SETTINGS_FILE)
+    logging.debug(' Vagrantfile successfully generated in %s', SETTINGS_FILE)
 
 
 def generate_resolv_file():
@@ -118,10 +127,10 @@ def generate_resolv_file():
 
 
 def main():
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
     if "PUNCHBOX_DIR" not in os.environ:
-        logging.fatal('PUNCHBOX_DIR environment variable is not set')
+        logging.fatal(' PUNCHBOX_DIR environment variable is not set')
         exit(1)
 
     parser = argparse.ArgumentParser()
@@ -131,8 +140,8 @@ def main():
     platform_config = load_platform_config(args.config)
     generate_vagrantfile(platform_config)
     if "punch" in platform_config:
-        generate_model(platform_config)
-        generate_deployment_settings()
+        model = generate_model(platform_config)
+        generate_deployment_settings(model)
         generate_resolv_file()
 
 
--- punchbox.sh
+++ punchbox.sh
@@ -8,9 +8,9 @@ wht=$'\e[1;37m'
 end=$'\e[0m'
 
 # filesystem
+TIME=$(date +%s)
 SCRIPT_PATH="$(realpath "$0")"
 DIR="$(dirname "${SCRIPT_PATH}")"
-LOGFILE="/tmp/punchbox.log"
 PUNCHBOX_CONF_DIR="${DIR}/configurations"
 
 # installation
@@ -23,14 +23,14 @@ PYTHON_ENV_INSTALLED_MARKER="${DIR}/.python_env_installed"
 conf_mode=""
 vagrant_mode=""
 deploy_mode=""
+deploy_component=""
+run_mode=""
 
 # FUNCTIONS
 function usage() {
-    printf "Description
+    printf "Syntax: punchbox [options]
 
-    Syntax: punchbox [options]
-
-    options:
+    Options:
 
     ${grn}help${end}
     Print this.
@@ -46,8 +46,10 @@ function usage() {
     ${cyn}vagrant${end} [start|stop|clean|reload]
     Execute the provided action on your vagrant boxes.
 
-    ${cyn}deploy${end}
-    Deploy the current settings on the running vagrant boxes.
+    ${cyn}deploy${end} [all|<component>] [--secured]
+    If 'all', deploy all punch components with the current settings on the running vagrant boxes.
+    Else deploy the provided component with the current settings on the running vagrant boxes.
+    If '--secured', also use the deployment secrets file with '-e @\$PUNCHPLATFORM_CONF_DIR/security/deployment_secrets.json'
 
     ${red}clean${end}
     Delete \$PUNCHPLATFORM_CONF_DIR content.
@@ -58,10 +60,33 @@ function usage() {
 
     ${wht}dev${end}
     Create symbolic links to \$PUNCH_DIR sources (i.e pp-punch). Set \$PUNCH_DIR first.
+
+
+${wht}Examples:${end}
+1. All in one - default TLS deployment
+punchbox run tls
+
+2. All in one - custom deployment
+punchbox run /tmp/my_conf.json
+
+3. Step by step - default configuration
+punchbox config default
+punchbox vagrant start
+punchbox deploy all
+
+4. Step by step - default TLS configuration
+punchbox config tls
+punchbox vagrant start
+punchbox deploy all --secured
+
+5. Step by step - custom configuration
+punchbox config /tmp/my_conf.json
+punchbox vagrant start
+punchbox deploy zookeeper,kafka
     "
 }
 
-function install_env() {
+function do_install() {
     # check dependencies
     [ -e "${HOME}/.ssh/id_rsa.pub" ] || { echo ".ssh/id_rsa.pub not found in user home directory. Maybe try running 'ssh-keygen' without specific option." 2>&1 && exit 42 ; }
 	which jq 1>/dev/null || { echo "jq command must be available on this node (deployer prerequisite)" 2>&1 && exit 11 ; }
@@ -86,8 +111,7 @@ function install_env() {
 }
 
 # arg 1 : configuration mode ('default' or  'tls') or a path to a punchbox config file
-function configure() {
-    echo "Log inside ${LOGFILE}"
+function do_configure() {
     punchbox_default_confs_dir="${PUNCH_DEPLOYER}/resources/punchbox"
 
     # pp-conf
@@ -95,30 +119,25 @@ function configure() {
         mkdir -p "${PUNCHPLATFORM_CONF_DIR}"
     fi
 
-    # parse the configuration
-    # it may be a supported mode like 'default' or 'tls'
+    # The configuration may be a default name like 'default', 'tls' or 'modsecurity' packaged inside the deployer dir
     # else it is a configuration path provided by the user
     if [ "$1" == "default" ]; then
-        # default conf
         config_file="${punchbox_default_confs_dir}/default/default_config.json"
     elif [ "$1" == "tls" ]; then
-        # default tls conf
         config_file="${punchbox_default_confs_dir}/tls/tls_config.json"
         # security dir with credentials and secrets
         cp -r "${punchbox_default_confs_dir}/tls/security" "${PUNCHPLATFORM_CONF_DIR}"
         echo "${grn}Security resources${end} moved to ${PUNCHPLATFORM_CONF_DIR}/security"
+    elif [ "$1" == "modsecurity" ]; then
+        config_file="${punchbox_default_confs_dir}/modsecurity/modsecurity_config.json"
     else
         # the conf_mode (i.e the argument is a path given by the user)
         config_file="$1"
     fi
 
     # generate deployment settings
-    python3 ${PUNCHBOX_PY} --config "${config_file}" >> "${LOGFILE}" 2>&1
-
-    if [ ! $? -eq 0 ]; then
-        echo "${red}FAIL: check ${LOGFILE} for error message"
-        exit 1
-    fi
+    python3 ${PUNCHBOX_PY} --config "${config_file}"
+    if [ ! $? -eq 0 ]; then exit 1; fi
 
     echo "${grn}Vagrantfile${end} generated in ${PUNCHBOX_BUILD_DIR}"
     echo "${grn}Deployment settings${end} generated in ${PUNCHPLATFORM_CONF_DIR}"
@@ -136,6 +155,7 @@ function do_vagrant(){
             vagrant reload
         elif [ "$1" == "clean" ]; then
             vagrant destroy -f
+            rm -rf "${PUNCHBOX_BUILD_DIR}/.vagrant" "${PUNCHBOX_BUILD_DIR}/Vagrantfile"
         else
             echo "${red}Unsupported vagrant option !${end}"
         fi
@@ -147,17 +167,31 @@ function do_vagrant(){
     cd "${DIR}"
 }
 
-function deploy() {
-    echo "Deploy using ${PUNCH_DEPLOYER}"
-    punchplatform-deployer.sh deploy -u vagrant
+# arg1 : 'all', or the punch component name to deploy (eg: kafka)
+# arg2 : '--secured' or "" (empty string)
+function do_deploy() {
+    deployment_secrets_file="${PUNCHPLATFORM_CONF_DIR}/security/deployment_secrets.json"
+    if [ "$1" == "all" ] && [ "$2" == "--secured" ]; then
+        echo "all secured"
+        punchplatform-deployer.sh --deploy -u vagrant -e @${deployment_secrets_file}
+    elif [ "$1" != all ] && [ "$2" == "--secured" ]; then
+        echo "not all secured"
+        punchplatform-deployer.sh --deploy -u vagrant -e @${deployment_secrets_file} -t "$1"
+    elif [ "$1" == all ] && [ "$2" != "--secured" ]; then
+        echo "all not secured"
+        punchplatform-deployer.sh --deploy -u vagrant
+    else
+        echo "not all not secured"
+        punchplatform-deployer.sh --deploy -u vagrant -t "$1"
+    fi
 }
 
-function clean_conf() {
+function do_clean() {
     echo "${red}Delete ${PUNCHPLATFORM_CONF_DIR}"
     rm -rf "${PUNCHPLATFORM_CONF_DIR}"
 }
 
-function development(){
+function do_dev_links(){
 
     if [ -z "${PUNCH_DIR}" ]; then
         # check for pp-punch dir next to punchbox_dir
@@ -184,7 +218,7 @@ function development(){
     echo "Created links to pp-punch in ${PUNCH_DEPLOYER}"
 }
 
-function environment() {
+function print_env() {
     echo "${cyn}Punchbox python dependencies${end}"
 	pip freeze
 	echo "${cyn}Punchbox shell environment${end}"
@@ -192,7 +226,7 @@ function environment() {
 }
 
 # PARSE OPTIONS
-install_env
+do_install
 source "${DIR}/.venv/bin/activate"
 source "${ACTIVATE_SH}"
 
@@ -204,6 +238,10 @@ while [[ $# -gt 0 ]]; do
         usage
         exit 0
         ;;
+    -r | --run | run)
+        run_mode=$2
+        shift 2
+        ;;
     -c | --config | config)
         conf_mode=$2
         shift 2
@@ -213,19 +251,25 @@ while [[ $# -gt 0 ]]; do
         shift 2
         ;;
     -d | --deploy | deploy)
-        deploy_mode=$2
-        shift 2
+        # args may be from 'deploy [all|<component>]' or 'deploy [all|<component>] --secured'
+        deploy_component="$2"
+        if [ ! -z "$3" ]; then
+            deploy_mode="$3"
+            shift 3
+        else
+            shift 2
+        fi
         ;;
     -c | --clean | clean)
-        clean_conf
+        do_clean
         exit 0
         ;;
     --dev | dev)
-        development
+        do_dev_links
         exit 0
         ;;
     --env | env)
-        environment
+        print_env
         exit 0
         ;;
     *) # unknown option
@@ -238,13 +282,19 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 # PROGRAM
-if [ ! -z "${deploy_mode}" ]; then
-    configure ${deploy_mode}
+if [ ! -z "${run_mode}" ]; then
+    do_configure ${run_mode}
     do_vagrant "start"
-    deploy ${deploy_mode}
+    secured_arg="none"
+    if [ "${run_mode}" == "tls" ]; then
+        secured_arg="--secured"
+    fi
+    do_deploy "all" "${secured_arg}"
 elif [ ! -z "${conf_mode}" ]; then
-    configure "${conf_mode}"
+    do_configure "${conf_mode}"
 elif [ ! -z "${vagrant_mode}" ]; then
     do_vagrant "${vagrant_mode}"
+elif [ ! -z "${deploy_component}" ]; then
+    do_deploy "${deploy_component}" "${deploy_mode}"
 fi
 
